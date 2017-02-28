7e9f83ab514e90b6c8b0413a8d3551e8fa4b3bdd
Ian Bolin <ibolin@myonc.org>
2017-02-28 16:27:51 -0500

    documented lift trigger class.

--- Steamworks/src/org/usfirst/frc/team5585/robot/triggers/LiftActive.java
+++ Steamworks/src/org/usfirst/frc/team5585/robot/triggers/LiftActive.java
@@ -5,14 +5,17 @@ import edu.wpi.first.wpilibj.buttons.Trigger;
 import org.usfirst.frc.team5585.robot.Robot;
 
 /**
- * trigger to determine whether lift system is active. Unused. due for removal.
- * @since 2/7/2017
+ * trigger to determine whether lift system is active. Unused. kept for illustration purposes. <br>
+ * Written on 2/7/2017
  * @author Ian Bolin
  * @category Trigger
- * @category Unused
  */
 public class LiftActive extends Trigger {
     
+	/**
+	 * gets the state of the lift.
+	 * @return The state of the lift.
+	 */
     public boolean get() {
         if(Robot.Lift.active) {
         	return true;
